fbeb166d802de54915fdd81dce96985379778186
Dayana <dayanaperzfls@gmail.com>
2024-09-04 18:43:26 -0600

    Realizacion de ejercicio 1 en index.php

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -7,7 +7,28 @@
     </head>
     <body>
         <?php
-        
+            echo '<h1> EJERCICIOS PRACTICA 5</h1>';
+
+            echo '<h2> Ejercicio 1</h2>';
+            echo 'Determina cuál de las siguientes variables son válidas y explica por qué:<br>';
+            echo "<br>";
+
+            //Definicion de variables
+            $_myvar = 'Soy la primera variable';
+            $_7var = 725;
+            /*myvar = 'Variable';*/
+            $myvar = 3.1416;
+            $var7 = true;
+            $_element1 = 'D';
+            /*$house*5 = '5690.1';*/
+
+            echo '<i>$_myvar</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore seguido de letras. $_myvar = '.$_myvar.'<br>';
+            echo '<i>$_7var</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_7var = '.$_7var.'<br>';
+            echo '<i>myvar</i>: <b>NO VALIDA.</b> No comienza con signo de dolar.'.'<br>';
+            echo '<i>$myvar</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras. $myvar = '.$myvar.'<br>';
+            echo '<i>$var7</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras y números. $var7 = '.$var7.'<br>';
+            echo '<i>$_element1</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_element1 = '.$_element1.'<br>';
+            echo '<i>$house*5</i>: <b>NO VALIDA.</b> Contiene un caracter especial (*), y deben ser numeros y/o letras.'.'<br>';
         ?>
     </body>
 </html>
