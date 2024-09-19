5705c8cd08cf72d066a872246c144b88e23f984a
Dayana <dayanaperzfls@gmail.com>
2024-09-18 22:52:19 -0600

    Creacion de get_productos_xhtml.php

--- practicas/p08/get_productos_xhtml.php
+++ practicas/p08/get_productos_xhtml.php
@@ -2,43 +2,39 @@
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 	<?php
-	if(isset($_GET['id']))
-		$id = $_GET['id'];
+	if (isset($_GET['tope'])) {
+		$tope = $_GET['tope'];
 
-	if (!empty($id))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'dayprzf24', 'marketzone');	
+		if (!empty($tope)) {
+			/** SE CREA EL OBJETO DE CONEXION */
+			@$link = new mysqli('localhost', 'root', 'dayprzf24', 'marketzone');
 
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+			/** comprobar la conexión */
+			if ($link->connect_errno) {
+				die('Falló la conexión: ' . $link->connect_error . '<br/>');
+			}
+
+			/** Consulta para obtener productos con unidades <= al tope */
+			if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) {
+				$rows = $result->fetch_all(MYSQLI_ASSOC);
+				$result->free();
+			}
+
+			$link->close();
 		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE id = '{$id}'") ) 
-		{
-			$row = $result->fetch_array(MYSQLI_ASSOC);
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
 	}
 	?>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Producto</title>
+		<title>Productos</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
 	<body>
-		<h3>PRODUCTO</h3>
+		<h3>PRODUCTOS</h3>
 
 		<br/>
 		
-		<?php if( isset($row) ) : ?>
+		<?php if (isset($rows) && count($rows) > 0) : ?>
 
 			<table class="table">
 				<thead class="thead-dark">
@@ -54,6 +50,7 @@
 					</tr>
 				</thead>
 				<tbody>
+					<?php foreach ($rows as $row) : ?>
 					<tr>
 						<th scope="row"><?= $row['id'] ?></th>
 						<td><?= $row['nombre'] ?></td>
@@ -64,13 +61,14 @@
 						<td><?= utf8_encode($row['detalles']) ?></td>
 						<td><img src=<?= $row['imagen'] ?> ></td>
 					</tr>
+					<?php endforeach; ?>
 				</tbody>
 			</table>
 
-		<?php elseif(!empty($id)) : ?>
+		<?php elseif (!empty($tope)) : ?>
 
 			 <script>
-                alert('El ID del producto no existe');
+                alert('No se encontraron productos con unidades menores o iguales a <?= $tope ?>');
              </script>
 
 		<?php endif; ?>
