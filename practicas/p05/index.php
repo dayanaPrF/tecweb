e4ca483a60fa0162a8b75a568ca84b64a98519c9
Dayana <dayanaperzfls@gmail.com>
2024-09-19 14:49:04 -0600

    Modificacion de errores de p05

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -21,13 +21,13 @@
             $_element1 = 'D';
             /*$house*5 = '5690.1';*/
 
-            echo '<i>$_myvar</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore seguido de letras. $_myvar = '.$_myvar.'<br>';
-            echo '<i>$_7var</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_7var = '.$_7var.'<br>';
-            echo '<i>myvar</i>: <b>NO VALIDA.</b> No comienza con signo de dolar.'.'<br>';
-            echo '<i>$myvar</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras. $myvar = '.$myvar.'<br>';
-            echo '<i>$var7</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras y números. $var7 = '.$var7.'<br>';
-            echo '<i>$_element1</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_element1 = '.$_element1.'<br>';
-            echo '<i>$house*5</i>: <b>NO VALIDA.</b> Contiene un caracter especial (*), y deben ser numeros y/o letras.'.'<br>';
+            echo '<p><i>$_myvar</i>: <b>VALIDA.</b> Comienza con signo de dólar, después un underscore seguido de letras. $_myvar = '.$_myvar.'</p>';
+            echo '<p><i>$_7var</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_7var = '.$_7var.'</p>';
+            echo '<p><i>myvar</i>: <b>NO VALIDA.</b> No comienza con signo de dolar.'.'</p>';
+            echo '<p><i>$myvar</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras. $myvar = '.$myvar.'</p>';
+            echo '<p><i>$var7</i>: <b>VALIDA.</b> Comienza con signo de dolar seguido de letras y números. $var7 = '.$var7.'</p>';
+            echo '<p><i>$_element1</i>: <b>VALIDA.</b> Comienza con signo de dolar, después un underscore, posteriormente números y letras. $_element1 = '.$_element1.'</p>';
+            echo '<p><i>$house*5</i>: <b>NO VALIDA.</b> Contiene un caracter especial (*), y deben ser numeros y/o letras.'.'</p>';
 
             //Liberar variables del ejercicio 1
             unset($_myvar);
@@ -45,7 +45,7 @@
             $b = 'MySQL';
             $c = &$a;
 
-            echo '<b>Contenido de las variables:</b><br>';
+            echo '<b>Contenido de las variables:</b><br />';
             echo '<ul><li>$a = '.$a.'</li>';
             echo '<li>$b = '.$b.'</li>';
             echo '<li>$c = '.$c.'</li>';
@@ -55,19 +55,19 @@
             $a = "PHP server";
             $b = &$a;
 
-            echo '<b>Contenido de las variables modificadas:</b><br>';
+            echo '<b>Contenido de las variables modificadas:</b><br />';
             echo '<ul><li>$a = '.$a.'</li>';
             echo '<li>$b = '.$b.'</li>';
             echo '<li>$c = '.$c.'</li>';
             echo '</ul>';
 
             //Descripcion de lo sucedido
-            echo "<b>Explicacion:</b><br>";
+            echo "<b>Explicacion:</b><br />";
             echo 'Las primeras asignaciones asignan directamente valores a las variables <i>$a, $b y $ c</i> 
             (donde $c contiene la referencia del contenido de $a por medio del operador &). En el segundo bloque se le asigna un 
             nuevo valor a $a, siendo este <b>PHP server</b>, posteriormente se le asigna el contenido de $a
             a la variable $b, y, ya que $c ya se le asignó el contenido de $a anteriormente, ahora se le asigna el 
-            nuevo contenido, por lo que todas las variables imprimen <b>PHP server</b><br>';
+            nuevo contenido, por lo que todas las variables imprimen <b>PHP server</b><br />';
 
 
             //Liberar variables del ejercicio 2
@@ -80,29 +80,29 @@
 
             //Definicion de variables
             $a = "PHP5";
-            echo 'Valor de $a= '.$a.'<br>';
+            echo 'Valor de $a= '.$a.'<br />';
 
             $z[] = &$a;
             echo 'Valor de $z[]=';
             print_r($z);
-            echo '<br>';
+            echo '<br />';
 
             $b = "5a version de PHP";
-            echo 'Valor de $b= '.$b.'<br>';
+            echo 'Valor de $b= '.$b.'<br />';
 
             $c = $b*10;
-            echo 'Valor de $c= '.$c.'<br>';
+            echo 'Valor de $c= '.$c.'<br />';
 
             $a .= $b;
-            echo 'Valor de $a= '.$a.'<br>';
+            echo 'Valor de $a= '.$a.'<br />';
 
             $b *= $c;
-            echo 'Valor de $b= '.$b.'<br>';
+            echo 'Valor de $b= '.$b.'<br />';
 
             $z[0] = "MySQL";
             echo 'Valor de $z[]=';
             var_dump($z);
-            echo '<br>';
+            echo '<br />';
 
             //Liberar variables del ejercicio 3
             unset($a);
@@ -230,9 +230,9 @@
             //EJERCICIO 7
             echo '<h2> Ejercicio 7</h2>';
 
-            echo 'Versión de PHP y Apache: '.$_SERVER['SERVER_SOFTWARE'].'<br>';
-            echo 'Versión del sistema operativo (servidor): '.$_SERVER['SERVER_NAME'].'<br>';
-            echo 'Idioma del navegador (cliente): '.$_SERVER['HTTP_ACCEPT_LANGUAGE'].'<br>';
+            echo 'Versión de PHP y Apache: '.$_SERVER['SERVER_SOFTWARE'].'<br />';
+            echo 'Versión del sistema operativo (servidor): '.$_SERVER['SERVER_NAME'].'<br />';
+            echo 'Idioma del navegador (cliente): '.$_SERVER['HTTP_ACCEPT_LANGUAGE'].'<br />';
 
 
 
