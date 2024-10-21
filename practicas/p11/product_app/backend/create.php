c400d7654f5c59141619e114a84bdcf80f3d578b
Dayana <dayanaperzfls@gmail.com>
2024-10-20 21:43:14 -0600

    Observacion de los codigos de p09_sin_jquery

--- practicas/p11/product_app/backend/create.php
+++ practicas/p11/product_app/backend/create.php
@@ -6,7 +6,6 @@ $producto = file_get_contents('php://input');
 if (!empty($producto)) {
     // SE TRANSFORMA EL STRING DEL JASON A OBJETO
     $jsonOBJ = json_decode($producto);
-    
     // Extraer datos del objeto JSON
     $nombre = $conexion->real_escape_string($jsonOBJ->nombre);
     $marca = $conexion->real_escape_string($jsonOBJ->marca);
@@ -15,14 +14,12 @@ if (!empty($producto)) {
     $detalles = $conexion->real_escape_string($jsonOBJ->detalles);
     $unidades = $conexion->real_escape_string($jsonOBJ->unidades);
     $ruta_imagen = $conexion->real_escape_string($jsonOBJ->imagen);
-
     // VALIDAR SI YA EXISTE EL PRODUCTO (nombre y eliminado = 0)
     $sql = "SELECT 1 FROM productos WHERE nombre = ? AND eliminado = 0";
     $stmt = $conexion->prepare($sql);
     $stmt->bind_param('s', $nombre);
     $stmt->execute();
     $stmt->store_result();
-
     if ($stmt->num_rows > 0) {
         echo json_encode(["error" => "El producto ya existe"]);
     } else {
