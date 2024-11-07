f214e575b44557f34e6d2cdd45d0488d4eed8267
Dayana <dayanaperzfls@gmail.com>
2024-11-06 22:50:06 -0600

    Se realizo la clase abstracta DataBase.php

---
+++ actividades/a07/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,15 @@
+<?php
+abstract class DataBase {
+
+    protected $conexion;
+
+    public function __construct($user, $pass, $db) {
+        $this -> conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+    }
+}
+?>
