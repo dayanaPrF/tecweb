66cb3c519a0c00f566085b11d3a6d46aab72a072
Dayana <dayanaperzfls@gmail.com>
2024-11-10 12:46:27 -0600

    Creacion de clases segun UML p13

---
+++ practicas/p13/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,32 @@
+<?php
+namespace TECWEB\MYAPI;
+
+abstract class DataBase {
+    protected $conexion;
+    protected $data = [];
+
+    public function __construct($db, $user, $pass) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+        
+        /**
+         * NOTA: si la conexión falló $conexion contendrá false
+         **/
+        if (!$this->conexion) {
+            die('¡Base de datos NO conectada!');
+        }
+        /*else {
+            echo 'Base de datos encontrada';
+        }*/
+    }
+
+    public function getData() {
+        // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+        return json_encode($this->data, JSON_PRETTY_PRINT);
+    }
+}
+?>
